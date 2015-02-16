2a1dddc6c3d9411cd45815701f37880262bdb4b6
BenjaminBini <benjamin.bini@gmail.com>
2015-02-16 09:50:29 +0100

    Add animation

--- public/app/controllers/mvAnswerController.js
+++ public/app/controllers/mvAnswerController.js
@@ -1,7 +1,11 @@
-angular.module('app').controller('mvAnswerController', function($scope, mvQuestion, mvQuestionService) {
+angular.module('app').controller('mvAnswerController', function($scope, mvQuestion, mvQuestionService, $location) {
 	$scope.answer = function (answer) {
 		console.log('Answer ' + answer + ' : ' + $scope.question.answers[answer].text);
 		$scope.results = mvQuestionService.getProportions($scope.question);
 		$scope.answer = function() {};
 	};
+
+	$scope.nextQuestion = function () {
+		$location.path('/questions/random');
+	}
 });
