c7c6c015fe0bc4184d8dbe3b3ac3cf45fbb1efb0
BenjaminBini <benjamin.bini@gmail.com>
2015-02-25 15:45:17 +0100

    Fix client js error if comment author deleted

--- public/app/filters/commentFilters.js
+++ public/app/filters/commentFilters.js
@@ -1,9 +1,9 @@
 angular.module('commentFilters', []).filter('authorCssClass', function () {
 	return function (comment, question) {
-		var answers = comment.author.answers;
-		if (!answers) {
+		if (!comment.author || !comment.author.answers) {
 			return;
 		}
+		var answers = comment.author.answers;
 		for (var i = 0; i < answers.length; i++) {
 			if (answers[i].question == question._id) {
 				if (answers[i].answer === 0) {
