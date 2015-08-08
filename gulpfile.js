85226852a6e63666ec6e1d3f63c5b9638d8e25de
BenjaminBini <benjamin.bini@gmail.com>
2015-08-08 15:45:29 +0200

    Add gulp watch task

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,59 @@ var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
 var stylus = require('gulp-stylus');
 var jade = require('gulp-jade');
+var watch = require('gulp-watch');
+var nodemon = require('gulp-nodemon');
 
 var DIST_DIR = './public/dist';
 var JS_DIR = DIST_DIR + '/js';
 var CSS_DIR = DIST_DIR + '/css';
 var VIEW_DIR = DIST_DIR + '/views';
 var DIRECTIVES_DIR = DIST_DIR + '/directives-views';
+var COMPILED_STYLUS_DIR = './public/css/';
+
+var JS_SRC = ['./public/vendor/jquery/dist/jquery.min.js',
+              './public/vendor/bootstrap/dist/js/bootstrap.min.js',
+              './public/vendor/toastr/toastr.min.js',
+              './public/vendor/angular/angular.min.js',
+              './public/vendor/angular-resource/angular-resource.min.js',
+              './public/vendor/angular-animate/angular-animate.min.js',
+              './public/vendor/angular-route/angular-route.min.js',
+              './public/vendor/angular-translate/angular-translate.min.js',
+              './public/vendor/angular-translate-loader-static-files/angular-translate-loader-static-files.min.js',
+              './public/vendor/angular-local-storage/dist/angular-local-storage.min.js',
+              './public/vendor/angular-ui-select/dist/select.js',
+              './public/vendor/odometer/odometer.js',
+              './public/vendor/angular-odometer-js/dist/angular-odometer.js',
+              './public/vendor/Chart.js/Chart.min.js',
+              './public/vendor/angular-chart.js/dist/angular-chart.min.js',
+              './public/vendor/ngDialog/js/ngDialog.min.js',
+              './public/vendor/angular-utils-pagination/dirPagination.js',
+              './public/vendor/angular-hotkeys/build/hotkeys.min.js',
+              './public/app/app.js',
+              './public/app/config/*.js',
+              './public/app/controllers/*.js',
+              './public/app/services/*',
+              './public/app/resources/*.js',
+              './public/app/models/*.js',
+              './public/app/directives/**/*.js',
+              './public/app/filters/**.js'];
+
+var CSS_SRC = ['./public/css/socicon.css',
+                './public/vendor/ngDialog/css/ngDialog.min.css',
+                './public/vendor/ngDialog/css/ngDialog-theme-default.min.css',
+                './public/vendor/angular-ui-select/dist/select.css',
+                './public/vendor/odometer/themes/odometer-theme-minimal.css',
+                './public/vendor/toastr/toastr.css',
+                './public/vendor/angular-chart.js/dist/angular-chart.css',
+                './public/vendor/angular-hotkeys/build/angular-hotkeys.min.css',
+                './public/css/site.css',
+                './public/css/mobile.css'];
+
+var VIEW_SRC = ['./public/app/views/**/*.jade'];
+
+var DIRECTIVE_SRC = ['./public/app/directives/**/*.jade'];
+
+var STYLUS_SRC = ['./public/css/*.styl'];
 
 gulp.task('default', function () {
   gulp.start('build');
@@ -22,48 +69,19 @@ gulp.task('build', function () {
   gulp.start('build-client');
 });
 
-gulp.task('build-client', function () {
-  gulp.start('clean-client-dist');
-  gulp.start('build-client-js');
-  gulp.start('build-client-css');
-  gulp.start('build-client-views');
-});
-
 gulp.task('clean-client-dist', function () {
   return gulp.src(DIST_DIR)
     .pipe(clean());
 });
 
-gulp.task('build-client-js', ['clean-client-dist'], function () {
-  gulp.src(['./public/vendor/jquery/dist/jquery.min.js',
-            './public/vendor/bootstrap/dist/js/bootstrap.min.js',
-            './public/vendor/toastr/toastr.min.js',
-            './public/vendor/angular/angular.min.js',
-            './public/vendor/angular-resource/angular-resource.min.js',
-            './public/vendor/angular-animate/angular-animate.min.js',
-            './public/vendor/angular-route/angular-route.min.js',
-            './public/vendor/angular-translate/angular-translate.min.js',
-            './public/vendor/angular-translate-loader-static-files/angular-translate-loader-static-files.min.js',
-            './public/vendor/angular-local-storage/dist/angular-local-storage.min.js',
-            './public/vendor/angular-ui-select/dist/select.js',
-            './public/vendor/odometer/odometer.js',
-            './public/vendor/angular-odometer-js/dist/angular-odometer.js',
-            './public/vendor/Chart.js/Chart.min.js',
-            './public/vendor/angular-chart.js/dist/angular-chart.min.js',
-            './public/vendor/ngDialog/js/ngDialog.min.js',
-            './public/vendor/angular-utils-pagination/dirPagination.js',
-            './public/vendor/angular-hotkeys/build/hotkeys.min.js',
-            './public/app/app.js',
-            './public/app/config/*.js',
-            './public/app/controllers/*.js',
-            './public/app/services/*',
-            './public/app/resources/*.js',
-            './public/app/models/*.js',
-            './public/app/directives/tags-input/tagsInput.js',
-            './public/app/directives/answer-button/answerButton.js',
-            './public/app/directives/loading-spinner/loadingSpinner.js',
-            './public/app/filters/commentFilters.js',
-            './public/app/filters/questionFilters.js'])
+gulp.task('build-client', ['clean-client-dist'], function () {
+  gulp.start('build-client-js');
+  gulp.start('build-client-css');
+  gulp.start('build-client-views');
+});
+
+gulp.task('build-client-js', function () {
+  gulp.src(JS_SRC)
     .pipe(sourcemaps.init())
     .pipe(concat('dilemme.js'))
     .pipe(gulp.dest(JS_DIR))
@@ -73,23 +91,14 @@ gulp.task('build-client-js', ['clean-client-dist'], function () {
     .pipe(gulp.dest(JS_DIR));
 });
 
-gulp.task('build-client-stylus', ['clean-client-dist'], function () {
-  return gulp.src(['./public/css/*.styl'])
+gulp.task('build-client-stylus', function () {
+  return gulp.src(STYLUS_SRC)
     .pipe(stylus())
-    .pipe(gulp.dest('./public/css/'));
+    .pipe(gulp.dest(COMPILED_STYLUS_DIR));
 });
 
 gulp.task('build-client-css', ['build-client-stylus'], function () {
-  gulp.src(['./public/css/socicon.css',
-            './public/vendor/ngDialog/css/ngDialog.min.css',
-            './public/vendor/ngDialog/css/ngDialog-theme-default.min.css',
-            './public/vendor/angular-ui-select/dist/select.css',
-            './public/vendor/odometer/themes/odometer-theme-minimal.css',
-            './public/vendor/toastr/toastr.css',
-            './public/vendor/angular-chart.js/dist/angular-chart.css',
-            './public/vendor/angular-hotkeys/build/angular-hotkeys.min.css',
-            './public/css/site.css',
-            './public/css/mobile.css'])
+  gulp.src(CSS_SRC)
     .pipe(sourcemaps.init())
     .pipe(concat('dilemme.css'))
     .pipe(gulp.dest(CSS_DIR))
@@ -99,11 +108,29 @@ gulp.task('build-client-css', ['build-client-stylus'], function () {
     .pipe(gulp.dest(CSS_DIR));
 });
 
-gulp.task('build-client-views', ['clean-client-dist'], function () {
-  gulp.src(['./public/app/views/**/*.jade'])
+gulp.task('build-client-views', function () {
+  gulp.src(VIEW_SRC)
     .pipe(jade())
     .pipe(gulp.dest(VIEW_DIR));
   gulp.src(['./public/app/directives/**/*.jade'])
     .pipe(jade())
     .pipe(gulp.dest(DIRECTIVES_DIR));
+});
+
+gulp.task('watch', function () {
+  gulp.start('watch-client');
+  gulp.start('watch-server');
+});
+
+gulp.task('watch-client', function () {
+  gulp.watch(JS_SRC, ['build-client-js']);
+  gulp.watch(STYLUS_SRC, ['build-client-css']);
+  gulp.watch(VIEW_SRC, ['build-client-views']);
+});
+
+gulp.task('watch-server', function () {
+  nodemon({
+    script: 'server.js',
+    watch: './server'
+  });
 });
