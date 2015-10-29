0dac9e0dc1f5e557f141ab71fd2a38613ad2103c
BenjaminBini <benjamin.bini@gmail.com>
2015-10-29 17:49:38 +0100

    Use bluebird promises for mongoose

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -2,6 +2,7 @@
  * Mongoose configuration
  */
 var mongoose = require('mongoose');
+var Promise = require('bluebird');
 var userSchema = require('../schemas/user.schema');
 var userData = require('../data/user.data');
 var suggestionSchema = require('../schemas/suggestion.schema');
@@ -16,6 +17,9 @@ module.exports = function() {
   mongoose.model('Suggestion', suggestionSchema.schema);
   mongoose.model('IpAnswer', ipAnswerSchema.schema);
   mongoose.model('Question', questionSchema.schema);
+
+  // Set mongoose promises to be bluebird
+  mongoose.Promise = Promise;
 
   // Mongodb connection
   mongoose.connect(process.env.MONGO_URI);
