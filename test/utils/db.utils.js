71fa01ef18e22fe8e95abc72e549c5f092230cc6
BenjaminBini <benjamin.bini@gmail.com>
2015-10-30 17:31:09 +0100

    Refactor pre-test actions

--- test/utils/db.utils.js
+++ test/utils/db.utils.js
@@ -2,11 +2,10 @@ var mongoose = require('mongoose');
 var userSchema = require('../../server/schemas/user.schema');
 var suggestionSchema = require('../../server/schemas/suggestion.schema');
 var questionSchema = require('../../server/schemas/question.schema');
-var userData = require('../../server/data/user.data');
-var suggestionData = require('../../server/data/suggestion.data');
-var questionData = require('../../server/data/question.data');
+var ipAnswerSchema = require('../../server/schemas/ip-answer.schema');
 var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
+var restore = require('mongodb-restore');
 var Promise = require('bluebird');
 require('dotenv').load();
 
@@ -14,33 +13,12 @@ module.exports = function() {
   process.env.NODE_ENV = 'test';
 
   beforeEach(function(done) {
+
+    // Use chaiAsPromised
     chai.use(chaiAsPromised);
 
-    function clearDB() {
-      for (var i in mongoose.connection.collections) {
-        mongoose.connection.collections[i].remove(function() {});
-      }
-      return Promise.resolve();
-    }
-
-    function populateDB() {
-      mongoose.model('User', userSchema.schema);
-      mongoose.model('Suggestion', suggestionSchema.schema);
-      mongoose.model('Question', questionSchema.schema);
-      // Create default data in the db
-      return userData.createDefaultEntries()
-      .then(questionData.createDefaultEntries)
-      .then(suggestionData.createDefaultEntries)
-      .catch(function(err) {
-        console.log(err);
-      });
-    }
-
-    function initDB(done) {
-      return clearDB().then(populateDB).then(function() {
-        return done();
-      });
-    }
+    // Use bluebird promises
+    mongoose.Promise = Promise;
 
     if (mongoose.connection.readyState === 0) {
       mongoose.connect(process.env.MONGO_TEST_URI, function(err) {
@@ -51,6 +29,30 @@ module.exports = function() {
       });
     } else {
       return initDB(done);
+    }
+
+    function populateDB() {
+      mongoose.model('User', userSchema.schema);
+      mongoose.model('Suggestion', suggestionSchema.schema);
+      mongoose.model('Question', questionSchema.schema);
+      mongoose.model('IpAnswer', ipAnswerSchema.schema);
+
+      // Create default data in the db
+      return new Promise(function(resolve) {
+        restore({
+          uri: process.env.MONGO_TEST_URI,
+          root: __dirname + '/../../server/data/sample',
+          drop: true,
+          dropCollections: true,
+          metadata: true,
+          callback: resolve
+        });
+
+      });
+    }
+
+    function initDB(done) {
+      return populateDB().then(done);
     }
 
   });
