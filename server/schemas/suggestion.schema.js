c6ab50fcd174d70f152b4ac70865470831916233
BenjaminBini <benjamin.bini@gmail.com>
2015-11-12 11:37:08 +0100

    Add 'use strict' to schemas

--- server/schemas/suggestion.schema.js
+++ server/schemas/suggestion.schema.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var mongoose = require('mongoose');
 var suggestionModel = require('../models/suggestion.model.js');
 
--- server/schemas/user.schema.js
+++ server/schemas/user.schema.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var mongoose = require('mongoose');
 var userModel = require('../models/user.model.js');
 
