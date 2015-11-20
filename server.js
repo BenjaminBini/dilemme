7de780a4be66431457c704852a19815b095b7aef
BenjaminBini <benjamin.bini@gmail.com>
2015-11-21 00:56:36 +0100

    Add comments to main file

--- server.js
+++ server.js
@@ -6,12 +6,16 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 
+// Configure DB connection and models
 require('./server/config/mongoose')();
 
+// Configure app (common middlewares)
 require('./server/config/express')(app);
 
+// Configure passport authentication methods
 require('./server/config/passport')();
 
+// Configure routes
 require('./server/config/routes')(app);
 
 // Log uncaught errors
