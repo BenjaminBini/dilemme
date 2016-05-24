dbcfeb24c14fdb7ffe757cd8a25c868126a7e963
BenjaminBini <benjamin.bini@gmail.com>
2016-05-24 17:15:44 +0200

    Add fake nodemailer transporter for unit testing

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var nodemailer = require('nodemailer');
+var stubTransport = require('nodemailer-stub-transport');
 var templates = require('../utils/mail-templates');
 var Promise = require('bluebird');
 
@@ -9,29 +10,37 @@ var Promise = require('bluebird');
  */
 module.exports.sendRequestNewPasswordMail = sendRequestNewPasswordMail;
 
-function sendRequestNewPasswordMail(language, to, token) {
+function sendRequestNewPasswordMail(language, to, token, doNotSendMail) {
+  if (language.toLowerCase() !== 'en' && language.toLowerCase() !== 'fr') {
+    language = 'en';
+  }
   var subject = templates.newPasswordRequest.subject[language];
   var text = templates.newPasswordRequest.body[language];
   text = text.replace('{{URL}}', process.env.ROOT_PATH + '/forgot-password/reset/' + token);
-  return _sendMail('Dilemme <benjamin@dilemme.io>', to, subject, text);
+
+  var transport = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASSWORD
+      }
+    });
+  if (doNotSendMail === true) {
+    transport = nodemailer.createTransport(stubTransport());
+  }
+
+  return _sendMail('Dilemme <benjamin@dilemme.io>', to, subject, text, transport);
 }
 
-function _sendMail(from, to, subject, body) {
+function _sendMail(from, to, subject, body, transport) {
   var mailOptions = {
     from: from,
     to: to,
     subject: subject,
     text: body
   };
-  var transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-      user: process.env.GMAIL_USER,
-      pass: process.env.GMAIL_PASSWORD
-    }
-  });
   return new Promise(function(resolve, reject) {
-    transporter.sendMail(mailOptions, function(error, info) {
+    transport.sendMail(mailOptions, function(error, info) {
       if (error) {
         reject(error);
       } else {
